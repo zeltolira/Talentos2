4b63386267842685a41fafdc38f5c659bf05517a
zeltolira <zelto16@hotmail.com>
2023-09-10 16:25:00 -0300

    inicia projeto

---
+++ src/main/java/com/lira/talento/TalentoApplication.java
@@ -0,0 +1,13 @@
+package com.lira.talento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lira/talento/TalentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lira.talento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
