7a1be6fe4a3d1eafb575bd3491f12cdb295cff64
zeltolira <zelto16@hotmail.com>
2023-09-10 16:38:36 -0300

    git commit -m "testa parte web. ok"

--- src/main/java/com/lira/talento/TalentoApplication.java
+++ src/main/java/com/lira/talento/TalentoApplication.java
@@ -2,9 +2,19 @@ package com.lira.talento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping
 public class TalentoApplication {
+
+	@GetMapping
+	public String getHomeTest() {
+		return "Já disse que te amo hoje?";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalentoApplication.class, args);
